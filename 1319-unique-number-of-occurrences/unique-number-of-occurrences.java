75271a5ba5e2683123836c0c0b1c1994d8aea648
AMIT DUTTA <amit8391810917@gmail.com>
2026-07-20 12:53:38 +0530

    Time: 2 ms (97.95%) | Memory: 43.5 MB (84.15%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,22 +1,36 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        // Map<Integer,Integer> mpp = new HashMap<>(2005);
+        // // Map<Integer,Integer> mpp = new HashMap<>(2005);
+        // // for(int ele : arr){
+        // //     mpp.put(ele,mpp.getOrDefault(ele,0)+1);
+        // // }
+
+        // int[] hash = new int[2005];
         // for(int ele : arr){
-        //     mpp.put(ele,mpp.getOrDefault(ele,0)+1);
+        //     hash[ele+1000]++;
         // }
 
-        int[] hash = new int[2005];
-        for(int ele : arr){
-            hash[ele+1000]++;
+        // Set<Integer> st = new HashSet<>();
+
+        // for(int ele : hash){
+        //     if(st.contains(ele))  return false;
+
+        //     else if(ele != 0)
+        //        st.add(ele);
+        // }
+        // return true;
+
+        Map<Integer, Integer> mpp = new HashMap<>();
+
+        for (int ele : arr) {
+            mpp.put(ele, mpp.getOrDefault(ele, 0) + 1);
         }
 
         Set<Integer> st = new HashSet<>();
-    
-        for(int ele : hash){
-            if(st.contains(ele))  return false;
-                
-            else if(ele != 0)
-               st.add(ele);
+
+        for (int cnt : mpp.values()) {
+            if (!st.add(cnt))
+                return false;
         }
         return true;
     }
